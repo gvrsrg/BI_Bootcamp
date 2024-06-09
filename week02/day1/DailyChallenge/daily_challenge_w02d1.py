b2cc364a2811f95e331dc659fe46df3e203971ef
gvrsrg <gvrsrg@gmail.com>
2024-06-09 15:52:08 +0300

    daily challenge week02 day1

---
+++ week02/day1/DailyChallenge/daily_challenge_w02d1.py
@@ -0,0 +1,19 @@
+import random
+string_length = 10
+
+input_string = input("Enter string:")
+if len(input_string) < string_length:
+    print("The string is not long enough.")
+elif len(input_string) > string_length:
+    print("The string is too long.")
+else:
+        
+    print(input_string[0]+input_string[string_length-1])
+
+    for i in range(string_length+1):
+        print(input_string[:i])
+    
+    shuffled_string = list(input_string)
+    random.shuffle(shuffled_string)
+    shuffled_string = ''.join(shuffled_string)
+    print(f"New string: {shuffled_string}")
