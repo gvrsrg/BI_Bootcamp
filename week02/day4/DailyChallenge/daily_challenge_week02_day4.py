dddb28b8ebc576f8eda19809dba6b6c5ae45ddae
gvrsrg <gvrsrg@gmail.com>
2024-06-14 02:07:44 +0300

    week02 day4 Daily Challenge

--- week02/day4/DailyChallenge/daily_challenge_week02_day4.py
+++ week02/day4/DailyChallenge/daily_challenge_week02_day4.py
@@ -1,7 +1,49 @@
-message = "a2w.e9r)s_df"
-for index, char in enumerate(message):
-    if not char.isalpha():
-        message = message.replace(char, " ")
+message = [
+    ["7","i","i"],
+    ["T","s","x"],
+    ["h","%","?"],
+    ["i"," ","#"],
+    ["s","M"," "],
+    ["$","a"," "],
+    ["#","t","%"],
+    ["^","r","!"]
+    ]
 
-message = message.replace("  ", " ")
-print(message)
+
+
+def transpose_matrix(matrix):
+    rows = len(message)
+    cols = len(message[0])
+    transposed_matrix = [[] for _ in message[0]]
+    
+
+    for row in matrix:
+        for index, value in enumerate(row):
+            transposed_matrix[index].append(value)
+    return transposed_matrix
+
+
+def reduce_matrix(matrix):
+    new_matrix = sum(matrix, [])
+    # for row in matrix:
+    #     line = ''.join(row)
+    #     new_matrix.append(line)
+
+    return ''.join(new_matrix)
+
+def decode_message(message):
+    for char in  (message):
+        if not char.isalpha():
+            message = message.replace(char, " ")
+    while message.find("  ")>0:
+        message = message.replace("  ", " ").strip()
+
+    return message
+
+new_matrix = transpose_matrix(message)
+encoded_message = reduce_matrix(new_matrix)
+print(encoded_message)
+
+decoded_message = decode_message(encoded_message)
+
+print(decoded_message)
