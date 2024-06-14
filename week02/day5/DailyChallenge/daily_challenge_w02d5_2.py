76b58d22413d64fedcd8c187d3c3b9d2c0129034
gvrsrg <gvrsrg@gmail.com>
2024-06-14 11:38:13 +0300

    daily challenge w02d5

--- week02/day5/DailyChallenge/daily_challenge_w02d5_2.py
+++ week02/day5/DailyChallenge/daily_challenge_w02d5_2.py
@@ -25,5 +25,8 @@ def longest_word(text):
         return word_list[index_max]
 
 
-print()
+print(longest_word("Margaret's toy is a pretty doll."))
 
+print(longest_word("A thing of beauty is a joy forever."))
+
+print(longest_word("Forgetfulness is by all means powerless!"))
