9726b2bb821404c54c9f1414050220ead1a4287b
gvrsrg <gvrsrg@gmail.com>
2024-06-16 13:05:33 +0300

    week03 day1 daily challenge expand

--- week03/day1/DailyChallenge/market.py
+++ week03/day1/DailyChallenge/market.py
@@ -16,7 +16,25 @@ class Farm():
             animals_info += f"{animal} : {count}\n"
         
         return title + animals_info + "\n    E-I-E-I-0!"
+    
+    def get_animals_types(self):
+        types = list(self.animals.keys())
+        types.sort()
+        return types
+    
+    def get_short_info(self):
+        animals_text = ', '.join([key + 's' if value > 1 else key for key, value in self.animals.items()])
+        return f"{self.owner}'s farm has {animals_text}."
+    
+# Add a method called get_animal_types to the Farm class. 
+# This method should return a sorted list of all the animal types (names) in the farm. 
+# With the example above, the list should be: ['cow', 'goat', 'sheep'].
 
+# Add another method to the Farm class called get_short_info. 
+# This method should return the following string: “McDonald’s farm has cows, goats and sheeps.”. 
+# The method should call the get_animal_types function to get a list of the animals.
+# Note : In English the plural form of the word “sheep” is sheep. 
+# But for the purpose of the exercise, let’s say that if there is more than 1 animal, an “s” should be added at the end of the word.    
 
 def main():
     macdonald = Farm("McDonald")
@@ -25,6 +43,11 @@ def main():
     macdonald.add_animal('sheep')
     macdonald.add_animal('goat', 12)
     print(macdonald.get_info())
+
+    print(macdonald.get_animals_types())
+
+    print(macdonald.get_short_info())
+
 
 if __name__ == "__main__":
     main()
