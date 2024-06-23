c0df8a8fa69342848e45369679e841cdd51ece16
gvrsrg <gvrsrg@gmail.com>
2024-06-23 16:18:55 +0300

    week04 day1 excerciseXP_Plus

---
+++ week04/day1/ExcerciseXP_Plus/insert.sql
@@ -0,0 +1,24 @@
+-- Here is the data:
+
+-- id	first_name	last_name	birth_date
+-- 1	Marc	Benichou	02/11/1998
+-- 2	Yoan	Cohen	03/12/2010
+-- 3	Lea	Benichou	27/07/1987
+-- 4	Amelia	Dux	07/04/1996
+-- 5	David	Grez	14/06/2003
+-- 6	Omer	Simpson	03/10/1980
+
+
+-- Insert
+-- Insert the data seen above to the students table. Find the most efficient way to insert the data.
+-- Insert your last_name, first_name and birth_date to the students table (Take a look at the id given).
+
+INSERT INTO students(first_name, last_name, birth_date)
+    VALUES
+    ('Marc', 'Benichou', '1998-02-11'),
+    ('Yoan', 'Cohen', '2010-03-12'),
+    ('Lea', 'Benichou', '1987-07-27'),
+    ('Amelia', 'Dux', '1996-04-07'),
+    ('David', 'Grez', '2003-06-14'),
+    ('Omer', 'Simpson', '1980-10-03'),
+    ('Sergey', 'Gavrilchick', '1977-08-14');
---
+++ week04/day1/ExcerciseXP_Plus/selects.sql
@@ -0,0 +1,25 @@
+-- Fetch all of the data from the table.
+-- Fetch all of the students first_names and last_names.
+-- For the following questions, only fetch the first_names and last_names of the students.
+-- Fetch the student which id is equal to 2.
+-- Fetch the student whose last_name is Benichou AND first_name is Marc.
+-- Fetch the students whose last_names are Benichou OR first_names are Marc.
+-- Fetch the students whose first_names contain the letter a.
+-- Fetch the students whose first_names start with the letter a.
+-- Fetch the students whose first_names end with the letter a.
+-- Fetch the students whose second to last letter of their first_names are a (Example: Leah).
+-- Fetch the students whose id’s are equal to 1 AND 3 .
+
+-- Fetch the students whose birth_dates are equal to or come after 1/01/2000. (show all their info).
+
+SELECT * FROM students;
+SELECT first_name, last_name FROM students;
+SELECT first_name, last_name FROM students WHERE id = 2;
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE 'a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a_'; 
+SELECT first_name, last_name FROM students WHERE id = 1 and id = 3;
+SELECT * FROM students WHERE birth_date >= '2000-01-01';
