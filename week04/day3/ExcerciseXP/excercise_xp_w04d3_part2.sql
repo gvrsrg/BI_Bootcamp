db4e80306d5cbf2a3604e06de31e353311ebf5fc
Sergey Gavrilchick <gvrsrg@gmail.com>
2024-06-25 23:09:47 +0300

    w04d3

---
+++ week04/day3/ExcerciseXP/excercise_xp_w04d3_part2.sql
@@ -0,0 +1,27 @@
+-- Use UPDATE to change the language of some films. Make sure that you use valid languages.
+update film set language_id = 4
+where title ilike '%japan%'
+
+
+-- Which foreign keys (references) are defined for the customer table?
+-- How does this affect the way in which we INSERT into the customer table?
+
+
+
+-- We created a new table called customer_review. Drop this table. Is this an easy step, or does it need extra checking?
+
+-- Find out how many rentals are still outstanding (ie. have not been returned to the store yet).
+
+-- Find the 30 most expensive movies which are outstanding (ie. have not been returned to the store yet)
+
+-- Your friend is at the store, and decides to rent a movie. He knows he wants to see 4 movies, 
+-- but he can’t remember their names. Can you help him find which movies he wants to rent?
+-- The 1st film : The film is about a sumo wrestler, and one of the actors is Penelope Monroe.
+
+-- The 2nd film : A short documentary (less than 1 hour long), rated “R”.
+
+-- The 3rd film : A film that his friend Matthew Mahan rented. He paid over $4.00 for the rental, 
+-- and he returned it between the 28th of July and the 1st of August, 2005.
+
+-- The 4th film : His friend Matthew Mahan watched this film, as well. 
+-- It had the word “boat” in the title or description, and it looked like it was a very expensive DVD to replace.
