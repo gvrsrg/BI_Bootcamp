aaa8cebf72cc7596ff828903a8621a33d302bcae
gvrsrg <gvrsrg@gmail.com>
2024-07-02 10:32:43 +0300

    rock-paper-scissors unfinished

--- week03/day5/Excercises_Rock_Paper_Scissors/game.py
+++ week03/day5/Excercises_Rock_Paper_Scissors/game.py
@@ -2,8 +2,18 @@
 import random
 
 class Game():
+    options = {
+        '1':"rock",
+        '2':"paper",
+        '3':"scissors"
+    }
+    
     def __init__(self):
-        pass
+        self.results = {
+        'win':0,
+        'loss':0,
+        'draw':0
+        }
     
 # get_user_item(self) – Ask the user to select an item (rock/paper/scissors).
 # Keep asking until the user has selected one of the items – use data validation and looping. Return the item at the end of the function.    
@@ -11,12 +21,16 @@ class Game():
     def get_user_item(self):
         valid_choice = False
         while not valid_choice:
-            user_item = input("Enter your choice (rock/paper/scissors): ")
-            valid_choice = user_item.lower() in ['rock', 'paper', 'scissors']
+            user_item = input("""Enter your choice 
+                              1. Rock
+                              2. Paper
+                              3. Scissors): 
+                              """)
+            valid_choice = user_item.lower() in ['1', '2', '3']
             if not valid_choice:
                 print("Invalid choice, please try again.")
         
-        return user_item.lower()
+        return Game.options[user_item]
             
         
     def get_computer_item(self):
@@ -25,17 +39,22 @@ class Game():
     
     def get_game_result(self, user_item, computer_item):
         if user_item == computer_item:
+            self.results['draw'] += 1
             return "draw"
         elif (user_item == 'rock' and computer_item == 'scissors') or \
         (user_item == 'scissors' and computer_item == 'paper') or \
         (user_item == 'paper' and computer_item == 'rock'):
+            self.results['win'] += 1
             return "win"
         else:
+            self.results['loss'] += 1
             return "loss"
     
     def play(self):
         user_item = self.get_user_item()
         computer_item = self.get_computer_item()
         result = self.get_game_result(user_item, computer_item)
+        return result
+    
 
         
