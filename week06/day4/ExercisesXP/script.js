34d7938bbd4e335e04c1160c5e952caaf650861a
gvrsrg <gvrsrg@gmail.com>
2024-07-10 14:52:52 +0300

    week06 day4 exercisesXP

---
+++ week06/day4/ExercisesXP/script.js
@@ -0,0 +1,32 @@
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+let marioJson = JSON.stringify(marioGame)
+
+console.log(marioJson)
+
+let prettyJson = JSON.parse(marioJson,undefined,2)
+
+console.log(prettyJson)
+
