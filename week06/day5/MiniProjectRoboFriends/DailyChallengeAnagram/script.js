acd9340df1d521970b1c743b7c59db0162ef318e
gvrsrg <gvrsrg@gmail.com>
2024-07-11 11:33:44 +0300

    week06 day5 anagram

---
+++ week06/day5/MiniProjectRoboFriends/DailyChallengeAnagram/script.js
@@ -0,0 +1,34 @@
+function makeSortedArray(str){
+    let arr = str.split('')
+    arr.sort()
+    let cleanArr = []
+    arr.forEach(element => {
+        if(element !== ' '){
+            cleanArr.push(element.toLowerCase())
+        };
+    });
+
+    return cleanArr
+}
+
+function isAnagram(str1, str2){
+    let arr1 = makeSortedArray(str1)
+    let arr2 = makeSortedArray(str2)
+
+    if(arr1.length !== arr2.length){
+        return false
+    }
+
+    arr1.forEach((element,index) => {
+        if(arr1[index] !== arr2[index]){
+            return false
+        }
+    })
+
+    return true
+}
+
+
+console.log(isAnagram("Astronomer","Moon starer"));
+console.log(isAnagram("School master","The classroom"));
+console.log(isAnagram("The Morse Code","Here come dots"));
