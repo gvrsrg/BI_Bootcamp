9a49a31188729946c944d7a0353ebb44607d8ef4
gvrsrg <gvrsrg@gmail.com>
2024-09-23 21:57:56 +0300

    port change

--- week12/day1/jwt-authentication/app.js
+++ week12/day1/jwt-authentication/app.js
@@ -12,11 +12,11 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5175", "http://localhost:3000"],
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`run on ${PORT}`);
 });
